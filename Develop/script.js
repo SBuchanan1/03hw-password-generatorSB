28e6837725bd2f99ab4b992e0d715a6cba75e81f
SBuchanan1 <syd.buch@gmail.com>
2020-10-27 14:20:29 -0400

    added number array

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,7 @@ var password = ""
 // Write password to the #password input
 function writePassword() {
   var passwordLengthPrompt = prompt("Please enter your desired password. It must be between 12 and 128 characters")
-  // var password = generatePassword();
+  numArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",]
   // var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
