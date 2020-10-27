6cd1742b34fd67e2daf96e29542f08ce4866af11
SBuchanan1 <syd.buch@gmail.com>
2020-10-27 14:18:42 -0400

    added variables to the js

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var password = ""
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordLengthPrompt = prompt("Please enter your desired password. It must be between 12 and 128 characters")
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
