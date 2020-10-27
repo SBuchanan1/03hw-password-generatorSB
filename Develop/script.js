9936a2d51365821a0df9b9ded92f2668657497a9
SBuchanan1 <syd.buch@gmail.com>
2020-10-27 14:28:37 -0400

    added letters array; upper and lowercase

--- Develop/script.js
+++ Develop/script.js
@@ -7,6 +7,8 @@ function writePassword() {
   var passwordLengthPrompt = prompt("Please enter your desired password. It must be between 12 and 128 characters")
   var numArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",]
   var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "?", ".", "+", "-", "_"]
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
   // var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
