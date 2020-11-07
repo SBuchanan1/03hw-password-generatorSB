455b7b1a9f6391c355b8f54b74ca96a0d14ddc2a
SBuchanan1 <syd.buch@gmail.com>
2020-11-07 14:13:13 -0500

    added proper functionality to passsword generator

--- Develop/script.js
+++ Develop/script.js
@@ -13,16 +13,39 @@ function writePassword() {
 
   // character sets
   var newCharSet = "";
-
-  var numArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",]
-  var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "?", ".", "+", "-", "_"]
   var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
   var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-  // var passwordText = document.querySelector("#password");
-  if (ifLowerCase) {
+  var numArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",]
+  var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "?", ".", "+", "-", "_"]
 
+
+  // var passwordText = document.querySelector("#password");
+  do {
+    var lowerCase = confirm("Include lowercase letters");
+    if (lowerCase) {
+      newCharSet += lowerCase;
+    }
+    var upperCase = confirm("Include uppercase letters");
+    if (upperCase) {
+      newCharSet += upperCase;
+    }
+    var numArr = confirm("Include numbers 0-9");
+    if (numArr) {
+      newCharSet += numArr;
+    }
+    var specialChar = confirm("Include special characters");
+    if (specialChar) {
+      specialChar += specialChar;
+    }
   }
-  passwordText.value = password;
+  while (newCharSet === "");
+
+  // for loop for character set and selects a random character to generate the password
+  for (var i = 0, n = newCharSet.length; i < passwordLength; i++) {
+    newPassword += newCharSet.charAt(Math.floor(Math.random() * n));
+  }
+  return newPassword;
+  // passwordText.value = password;
 
 }
 
